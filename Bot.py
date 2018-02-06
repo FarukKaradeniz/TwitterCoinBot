31a201962194f653d3b850bc8734566e97d550f9
Faruk Karadeniz <krdnzomer@gmail.com>
2018-02-06 22:01:20 +0300

    Initial Commit

---
+++ Bot.py
@@ -0,0 +1,36 @@
+import requests
+import simplejson as json
+import tweepy
+import time
+
+
+# Verilen json text'inden atilacak twiti olustur
+def createTweet(text):
+    info = json.loads(text)
+    string = ""
+    for item in info:
+        string += "#{} ${:.6} {:>6}%\n" \
+            .format(item['symbol'], item['price_usd'], item['percent_change_24h'])
+        # tweet.append(string)
+    return string
+
+
+# Json dosyasindan twitter access token bilgilerini al
+with open("TwitterApiKeys.json") as file:
+    twitterKeys = json.load(file)
+    file.close()
+
+# Tweepy modulunu ayarla
+oauth = tweepy.OAuthHandler(twitterKeys['consumer_key'], twitterKeys['consumer_secret'])
+oauth.set_access_token(twitterKeys['access_token'], twitterKeys['access_token_secret'])
+api = tweepy.API(oauth)
+
+# CoinMarketCap.com Api
+# Api'den gelen sonuclari her 2 saatte bir tweet at
+while True:
+    params = {"limit": "10"}
+    r = requests.get("https://api.coinmarketcap.com/v1/ticker/", params=params)
+    tweet = createTweet(r.text)
+    print(tweet)
+    time.sleep(60 * 60 * 2)  # Sleep for 2 hours
+    api.update_status(tweet)
---
+++ README.md
@@ -0,0 +1,21 @@
+# Uygulamanın Kurulumu
+Uygulamanın çalışması için **https://apps.twitter.com/** adresinden twitter uygulaması oluşturunuz. Oluşturduktan sonra uygulamanız için;
+- Consumer Key
+- Consumer Secret
+- Access Token
+- Access Token Secret
+
+Bilgilerini alınız. Bu bilgileri aldıktan sonra bunları 
+```json
+{
+  "consumer_key": "CONSUMER KEY GOES HERE",
+  "consumer_secret" : "CONSUMER SECRET GOES HERE",
+  "access_token" : "ACCESS TOKEN GOES HERE",
+  "access_token_secret" : "ACCESS TOKEN SECRET GOES HERE"
+}
+```
+olacak şekilde _TwitterApiKeys.json_ dosyasına kaydedin.
+Bütün bu işlemleri yaptıktan sonra script'i çalıştırabilirsiniz.
+
+## Örnek tweet
+![Tweet](./tweet.PNG)
